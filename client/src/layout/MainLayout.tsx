43f4ae4a05f1671da4a4b473fa44b902cdcfa073
lxrd-mgcini <ngwenyamgcini234@gmail.com>
2025-04-23 01:42:20 +0200

    Fix: Required types for the MainLayout Component

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,4 +1,10 @@
-export default function MainLayout({ children }) {
+import { ReactNode } from "react";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export default function MainLayout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen max-w-full pl-4 pr-4 sm:pl-8 sm:pr-8">
       {children}
